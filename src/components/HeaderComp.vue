6cb3ed538be3f404079cc84dc6a1e90adf691dd3
OHNIMUS-Leopold <leopold.ohnimus@edu.univ-fcomte.fr>
2024-02-11 04:08:37 +0100

    animation du theme switcher

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -2,10 +2,13 @@
 import { RouterLink } from 'vue-router'
 import { ref, onMounted } from 'vue';
 
+const isRotated = ref(false);
+
 const themes = ['theme-dark1', 'theme-dark2']; 
 const currentTheme = ref(themes[Math.floor(Math.random() * themes.length)]);
 
 function toggleTheme() {
+  isRotated.value = !isRotated.value;
   const nextTheme = themes.find(theme => theme !== currentTheme.value);
   if (nextTheme) {
     currentTheme.value = nextTheme;
@@ -26,7 +29,50 @@ onMounted(() => {
         </nav>
 
         <div>
-            <button @click="toggleTheme">Alterner Thème</button>
+            <button class="switch-btn" :class="{ rotated: isRotated }" @click="toggleTheme"></button>
         </div>
     </header>
 </template>
+
+
+
+<style scoped>
+
+.switch-btn {
+  width: 32px;
+  height: 32px;
+  background-color: gray;
+  border: 2px solid black;
+  
+  position: relative;
+
+  transition: transform 0.2s ease;
+  overflow: hidden;
+}
+
+.switch-btn.rotated {
+  transform: rotate(180deg);
+}
+
+.switch-btn::after {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 0;
+  height: 0;
+  border-top: 15px solid black; 
+  border-right: 15px solid transparent; 
+  border-bottom: 15px solid transparent; 
+  border-left: 15px solid black;   
+  transition: all 0.2s ease; 
+}
+
+.switch-btn:hover::after {
+  border-top: 23px solid black; 
+  border-right: 23px solid transparent; 
+  border-bottom: 23px solid transparent; 
+  border-left: 23px solid black;   
+}
+
+</style>
