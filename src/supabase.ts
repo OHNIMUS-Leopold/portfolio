5959d4e90feb97e15ab9e84bb7b6f03b1c18d1a1
OHNIMUS-Leopold <leopold.ohnimus@edu.univ-fcomte.fr>
2024-02-17 14:42:34 +0100

    mise en place de supabase

---
+++ src/supabase.ts
@@ -0,0 +1,15 @@
+import { createClient } from '@supabase/supabase-js'
+import { ref } from 'vue'
+// import { type Database } from '@/supabase-types'
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+globalThis.supabase = supabase
+
+// const { data } = await supabase.auth.getUser()
+// export const user = ref(data.user)
+// supabase.auth.onAuthStateChange( (evt,session)=>{
+//     user.value = session?.user ?? null
+// })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+import { supabase } from '@/supabase';
+console.log('supabase', supabase);
+</script>
+
 <template>
     
   <div>
